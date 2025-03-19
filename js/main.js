3b8d0fa753eb42bc58fb5104a4543c7671adc664
dongyeon <ldy1951123@gmail.com>
2025-03-19 18:45:39 +0900

    탭 기능 더 간단하게 구현하는 방법 & Array와 object 자료형 & Array, object 데이터 바인딩

--- js/main.js
+++ js/main.js
@@ -25,11 +25,39 @@
 //});
 
 // 위 코드를 반복문으로 재사용한 코드
-for(let i = 0 ; i < $('.tab-button').length ; i++) {
-    $('.tab-button').eq(i).on('click', function() {
+//for(let i = 0 ; i < $('.tab-button').length ; i++) {
+//    $('.tab-button').eq(i).on('click', function() {
+//        $('.tab-button').removeClass('orange');
+//        $('.tab-button').eq(i).addClass('orange');
+//        $('.tab-content').removeClass('show');
+//        $('.tab-content').eq(i).addClass('show');
+//    });
+//}
+
+// 위 코드를 축약하는 방법
+
+function 탭열기(숫자) {
+    $('.tab-button').eq(숫자).on('click', function() {
         $('.tab-button').removeClass('orange');
-        $('.tab-button').eq(i).addClass('orange');
+        $('.tab-button').eq(숫자).addClass('orange');
         $('.tab-content').removeClass('show');
-        $('.tab-content').eq(i).addClass('show');
+        $('.tab-content').eq(숫자).addClass('show');
     });
 }
+
+//for(let i = 0 ; i < $('.tab-button').length ; i++) {
+//    탭열기(i);
+//}
+
+$('.list').click(function(e) {
+    // 지금 누른게 버튼 0이면, 탭열기(0)
+    if ($(e.target).is($('.tab-button')[0])) {
+        탭열기(0);
+    }
+    else if ($(e.target).is($('.tab-button')[1])) {
+        탭열기(1);
+    }
+    else if ($(e.target).is($('.tab-button')[2])) {
+        탭열기(2);
+    }
+});
